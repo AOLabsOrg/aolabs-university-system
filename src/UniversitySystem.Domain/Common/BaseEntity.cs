61bc8d5677bbd64555a4504e1ef85897f96052d9
abdulrahman <abdulrahmn398@outlook.com>
2025-05-09 18:17:45 +0300

    Add BaseEntity and University classes with licensing info
    Introduce an abstract class `BaseEntity` with an `Id` property
    to serve as a base for other entities. Create a `University`
    class that inherits from `BaseEntity`, including properties
    for `Name`, `Location`, and a collection of `Faculties`.
    Add copyright and MIT License information to both files.

---
+++ src/UniversitySystem.Domain/Common/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Copyright (c) 2025 AOLabs
+ * This file is part of the AOLabs University System project.
+ *
+ * Licensed under the MIT License.
+ * You may obtain a copy of the License at:
+ * https://opensource.org/licenses/MIT
+ *
+ * You are free to use, modify, and distribute this file
+ * under the terms of the license.
+ */
+
+namespace UniversitySystem.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+}
---
+++ src/UniversitySystem.Domain/Entities/University.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Copyright (c) 2025 AOLabs
+ * This file is part of the AOLabs University System project.
+ *
+ * Licensed under the MIT License.
+ * You may obtain a copy of the License at:
+ * https://opensource.org/licenses/MIT
+ *
+ * You are free to use, modify, and distribute this file
+ * under the terms of the license.
+ */
+
+using UniversitySystem.Domain.Common;
+
+namespace UniversitySystem.Domain.Entities;
+
+public class University : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Location { get; set; }
+
+    public ICollection<Faculty> Faculties { get; set; } = new List<Faculty>();
+}
