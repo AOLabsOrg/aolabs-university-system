70514e5fca2093aa6197937d606b535e6fd8cd80
abdulrahman <abdulrahmn398@outlook.com>
2025-05-10 11:40:06 +0300

    Add Department class to manage university departments
    This commit introduces a new file, `Department.cs`, which includes a copyright notice and licensing information. The `Department` class inherits from `BaseEntity` and defines properties for `Name`, `FacultyId`, and navigation properties for `Faculty` and `Specializations`. This structure supports the management of departments within the University System project, establishing relationships with faculty and specializations.

---
+++ src/UniversitySystem.Domain/Entities/Department.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Copyright (c) 2025 AOLabs
+ * This file is part of the AOLabs University System project.
+ *
+ * Licensed under the MIT License.
+ * You may obtain a copy of the License at:
+ * https://opensource.org/licenses/MIT
+ *
+ * You are free to use, modify, and distribute this file
+ * under the terms of the license.
+ */
+
+using UniversitySystem.Domain.Common;
+
+namespace UniversitySystem.Domain.Entities;
+
+public class Department : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+
+    // Foreign key to Faculty
+    public Guid FacultyId { get; set; }
+    public Faculty Faculty { get; set; } = default!;
+
+    // Navigation to Specializations
+    public ICollection<Specialization> Specializations { get; set; } = new List<Specialization>();
+}
