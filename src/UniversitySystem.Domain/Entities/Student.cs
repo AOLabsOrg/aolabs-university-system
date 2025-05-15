60c2e2cb7009d98df7e8d7590c40efbc7ce4555e
abdulrahman <abdulrahmn398@outlook.com>
2025-05-16 00:26:11 +0300

    Add copyright notice and refactor Student class
    Updated `Student.cs` to include copyright and licensing information.
    Refactored the `Student` class properties to ensure proper initialization with default values.
    Retained foreign key comments and improved formatting for clarity.

--- src/UniversitySystem.Domain/Entities/Student.cs
+++ src/UniversitySystem.Domain/Entities/Student.cs
@@ -1,40 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Copyright (c) 2025 AOLabs
+ * This file is part of the AOLabs University System project.
+ *
+ * Licensed under the MIT License.
+ * You may obtain a copy of the License at:
+ * https://opensource.org/licenses/MIT
+ *
+ * You are free to use, modify, and distribute this file
+ * under the terms of the license.
+ */
+
 using UniversitySystem.Domain.Common;
 using UniversitySystem.Domain.Enums;
 
-namespace UniversitySystem.Domain.Entities
+namespace UniversitySystem.Domain.Entities;
+
+public class Student : BaseEntity
 {
-    public class Student : BaseEntity
-    {
-        public string FullName { get; set; }
-        public string NationalId { get; set; }
-        public string Email { get; set; }
-        public string Gender { get; set; }
-        public GpaType Gpa {  get; set; }
-        public DateTime BirthDate { get; set; }
-        public int AcademicYear { get; set; }
+    public string FullName { get; set; } = null!;
+    public string NationalId { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Gender { get; set; } = null!;
+    public GpaType Gpa { get; set; }
+    public DateTime BirthDate { get; set; }
+    public int AcademicYear { get; set; }
 
-        // Foreign key to Department
-        public Guid DepartmentId { get; set; }
-        public Department Department { get; set; } = default!;
+    // Foreign key to Department
+    public Guid DepartmentId { get; set; }
+    public Department Department { get; set; } = default!;
 
-        // Foreign key to University
-        public Guid UniversityId { get; set; }
-        public University University { get; set; } = default!;
-        
-        // Foreign key to Faculty
-        public Guid FacultyId { get; set; }
-        public Faculty Faculty{ get; set; } = default!;
+    // Foreign key to University
+    public Guid UniversityId { get; set; }
+    public University University { get; set; } = default!;
 
-        // Foreign key to Specialization
-        public Guid SpecializationId { get; set; }
-        public Specialization Specialization { get; set; } = default!;
+    // Foreign key to Faculty
+    public Guid FacultyId { get; set; }
+    public Faculty Faculty { get; set; } = default!;
 
-    }
+    // Foreign key to Specialization
+    public Guid SpecializationId { get; set; }
+    public Specialization Specialization { get; set; } = default!;
 
-    
 }
+
+
