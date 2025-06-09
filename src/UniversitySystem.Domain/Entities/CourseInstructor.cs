1f30177c790aba4dd8f941d14db6b21453f91830
Mohamed Morsi <777medooo@gmail.com>
2025-06-09 18:56:59 +0300

    Fix: Solved issue #28 by creating CourseInstructor entity

---
+++ src/UniversitySystem.Domain/Entities/CourseInstructor.cs
@@ -0,0 +1,21 @@
+﻿using UniversitySystem.Domain.Common;
+
+namespace UniversitySystem.Domain.Entities;
+
+public class CourseInstructor
+{
+    public Guid CourseId { get; set; }
+    public Guid InstructorId { get; set; }
+
+    // Navigation Properties
+    public Course Course { get; set; } = default!;
+    public Instructor Instructor { get; set; } = default!;
+
+    public CourseInstructor(Guid courseId, Guid instructorId)
+    {
+        CourseId = courseId;
+        InstructorId = instructorId;
+    }
+    private CourseInstructor() { }
+
+}
