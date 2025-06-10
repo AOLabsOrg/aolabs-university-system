2dee26e524fcf90ad8a50c3f117a8b22d8113700
Mohamed Morsi <777medooo@gmail.com>
2025-06-10 21:59:01 +0300

    Fix: Solved issue 37 by creating SpecializationConfiguration

---
+++ src/UniversitySystem.Infrastructure/SpecializationConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using UniversitySystem.Domain.Common;
+
+namespace UniversitySystem.Domain.Entities;
+
+public class SpecializationConfiguration : IEntityTypeConfiguration<Specialization>
+{
+    public void Configure(EntityTypeBuilder<Specialization> builder)
+    {
+        builder.Property(s => s.Name)
+               .HasMaxLength(100)
+               .IsRequired();
+
+        builder.HasOne(s => s.Department)
+            .WithMany(d => d.Specializations)
+            .HasForeignKey(s => s.DepartmentId);
+
+        builder.HasMany(s => s.Students)
+            .WithOne(std => std.Specialization)
+            .HasForeignKey(std => std.SpecializationId);
+    }
+}
